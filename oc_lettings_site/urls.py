5636760b48cf7d8d0a76254bb86e71fd8bd023bb
assakali <assakali.mehdi@gmail.com>
2025-05-23 10:16:25 +0200

    Initialisation de docker avec l'implémentation du dockerfile et du dockerignore

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from oc_lettings_site import views as core_view
 
 urlpatterns = [
@@ -11,3 +12,5 @@ urlpatterns = [
     path('test500', core_view.test_500),
     path("sentry-error/", core_view.test_error),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
