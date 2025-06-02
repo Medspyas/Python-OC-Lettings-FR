0dcc3e2316d842a05a72d6251a62f7618d960ecc
assakali <assakali.mehdi@gmail.com>
2025-06-02 13:15:26 +0200

    Modification chemin conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,6 @@ project = "oc-lettings"
 copyright = "2025, mehdi"
 author = "mehdi"
 
-
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
